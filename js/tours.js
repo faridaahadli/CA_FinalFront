ae88f53406b58ceb4a90ae8af4ac5dd37e8ff6cb
Farida Ahadli <faridaahadli59@gmail.com>
2020-01-29 00:57:28 +0400

    Filters responsive add

--- js/tours.js
+++ js/tours.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+// Pagination
+
+
     // Datetime Filter
     $(function() {
         $('input[name="daterange"]').daterangepicker({
@@ -12,16 +15,23 @@ $(document).ready(function(){
     // Price filter
 
     $( function() {
-        $( "#slider-range" ).slider({
+        $( ".slider-range" ).slider({
           range: true,
           min: 0,
           max: 500,
           values: [ 75, 300 ],
           slide: function( event, ui ) {
-            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            $( ".amount" ).val(   ui.values[ 0 ] + " azn"+" - " + ui.values[ 1 ] +" azn");
           }
         });
-        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-          " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+        $( ".amount" ).val(   $( ".slider-range" ).slider( "values", 0 )  +" azn"+
+          " - " + $( ".slider-range" ).slider( "values", 1 )+" azn" );
       });
+
+      // Filter button click
+      $(".fltBtn button").click(function(){
+
+  $(".overlay").toggleClass("aftOver")
+
+      })
 })
